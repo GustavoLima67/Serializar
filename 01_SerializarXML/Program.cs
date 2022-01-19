74d34a032afd7aa9be27d39c58ede5fc42e1a3c3
Gustavo <lgusta333@gmail.com>
2022-01-18 22:26:18 -0300

    Initial commit

---
+++ 01_SerializarXML/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using 00_Biblioteca;
+using System.Xml.Serialization;
+
+namespace _01_SerializarXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ sadsa/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sadsa
+{
+    public class Usuario
+    {
+        private string name { get; set; }
+        private string cpf { get; set; }
+        private string email { get; set; }
+    }
+}
